e9d824a32ab444572a2367b3a51f1a750cf33f84
ManishDongarwar <130134075+ManishDongarwar@users.noreply.github.com>
2023-04-07 14:27:15 +0530

    Update Basic.java

--- Newproject/src/Array/Basic.java
+++ Newproject/src/Array/Basic.java
@@ -9,22 +9,23 @@ public static void main(String[]args)
 
 {
 
-   int a[] = { 20 , 10, 40, 30};
+   int a[] = { 20 , 10, 40, 30 , 75};
 
 
           a[0] = 20;
           a[1] = 10;
           a[2] = 40;
           a[3] = 30;
+	  a[4] = 75;   //manager did
 
-for (int i = 0; i <=3;i++)
+for (int i = 0; i <=4;i++)
 {
   System.out.println(a[i]);
 }
     System.out.println();
    Arrays.sort (a);
 
-for (int i = 0; i <=3;i++)
+for (int i = 0; i <=4;i++)
 {
 	System.out.println(a[i]);
 }
